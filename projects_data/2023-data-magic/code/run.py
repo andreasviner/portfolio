9ab6f783e39c810cdfbe521a395552f6d84bce07
Andreas Lindeman <andreas.e.lindeman@gmail.com>
2026-05-31 13:52:14 +0200

    Project updates

---
+++ projects_data/2023-data-magic/code/run.py
@@ -0,0 +1,8 @@
+import PIL.Image,time
+A = [PIL.Image.open("img1.png").load(),*[l for l in '_)]⌐«·k",‼M/z[≡aWα^@*L\'~.π(!ª x1ª'],time.time()]
+for i in range(5068444):
+    A[32]+=("0"*16+bin(((A[0][(i//1905)%2663,i%1905][0]&0x0f)<<12)+((A[0][(i//1905)%2663,i%1905][1]&0x0f)<<8)+((A[0][(i//1905)%2663,i%1905][2]&0x0f)<<4)+(A[0][(i//1905)%2663,i%1905][3]&0x0f))[2:])[-16:]
+    while len(A[32])>11:A[32:34]=[A[32][6+5*int(A[32][0]):],A[33]+A[int(A[32][1:6],2)]+(int(A[32][6:11],2)*int(A[32][0]))*A[int(A[32][1:6],2)]]
+    if len(A[33])>6288:print("\033[F"*49);[print(A[33][ii*131:(ii+1)*131]) for ii in range(48)];time.sleep(max(A[34]+1/25-time.time(),0));A[33:]=[A[33][6288:],time.time()]
+
+
